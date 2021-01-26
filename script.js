ea0291c87cfa5a2e4d4828930fc0b1337171cade
Nathan <nmwachholz@gmail.com>
2021-01-26 14:38:19 -0800

    Add modal popup

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hi");
+
+
+document.querySelectorAll(".gallery-item").forEach(
+    (item) => {
+        item.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            let modalBack = document.createElement("div");
+            modalBack.classList.add("modal-back");
+
+            let modal = document.createElement("div");
+            modal.classList.add("modal");
+
+            const media = item.firstElementChild;
+
+            let mediaClone = media.cloneNode(true);
+
+            mediaClone.addEventListener("click", (event) => {
+                event.stopPropagation();
+            })
+
+            mediaClone.controls = true;
+            mediaClone.autoplay = true;
+            modal.appendChild(mediaClone);
+
+            modalBack.appendChild(modal);
+
+            modalBack.addEventListener("click", () => {
+                modalBack.remove();
+            })
+
+            document.body.append(modalBack);
+        })
+    }
+);
